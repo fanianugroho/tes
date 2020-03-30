14a8143529155a85a91559ca1ee519bf453a69ce
fanianugroho <hanifanianugroho@gmail.com>
2020-03-30 15:39:24 +0700

    Updates

---
+++ precache-manifest.6fb90f547a012645b010a91803f00c60.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3112f6afacd94d81579352339d471c",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/index.html"
+  },
+  {
+    "revision": "38f4b272c58a4e2ac9ff",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "9e1fa1fcf6f589ae1f31",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/css/main.4128893c.chunk.css"
+  },
+  {
+    "revision": "38f4b272c58a4e2ac9ff",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/js/2.1196d7f5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/js/2.1196d7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1fa1fcf6f589ae1f31",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/js/main.3af1283e.chunk.js"
+  },
+  {
+    "revision": "366e35f5bd711f7920b1",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/js/runtime-main.0a7ec760.js"
+  },
+  {
+    "revision": "6fd7f59fc0ea1f9536084a95fc2c6ff0",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/1.6fd7f59f.JPG"
+  },
+  {
+    "revision": "6db70d24bc32be91cd9902a8c740e484",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/2.6db70d24.jpeg"
+  },
+  {
+    "revision": "0760cb8148a239cbdbcfbe95ec482b8d",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/3.0760cb81.jpg"
+  },
+  {
+    "revision": "8439d9017baa52d37c23871c2dc382c8",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/4.8439d901.jpg"
+  },
+  {
+    "revision": "4f17a342bea9d2b55ef8e50a25409a03",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/5.4f17a342.JPG"
+  },
+  {
+    "revision": "bee2ee03f600c743a79112a7981fb956",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/6.bee2ee03.jpeg"
+  },
+  {
+    "revision": "137c2e79e901d38d321a4048fecc1492",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/picture.137c2e79.png"
+  },
+  {
+    "revision": "e5a65f399ec217921e6f966190ee320b",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/picture2.e5a65f39.png"
+  },
+  {
+    "revision": "3a1a08a749a4d179a6bb62139704508c",
+    "url": "/WEB3A-15108-Hani-Fania-REACT/static/media/picture3.3a1a08a7.png"
+  }
+]);
